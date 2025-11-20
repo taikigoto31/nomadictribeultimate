754c3224046edd9e5e9d118abf3df5df6ac88741
taikigoto31 <taikigoto.31@gmail.com>
2025-11-20 15:43:58 +0900

    fix: ファイル構造を修正

---
+++ js/data/players-handler.js
@@ -0,0 +1,332 @@
+// HANDLERポジションの選手データ
+// 画像パス: images/players/handler/player_{id}.jpg (メイン画像)
+//          images/players/handler/player_{id}_thumb1.jpg, thumb2.jpg, thumb3.jpg (Q&A用、最大3枚)
+// IDは選手ごとに重複しないように設定してください (例: 1, 2, 3...)
+
+const handlerPlayers = [
+    {
+        id: 7,
+        name: "佐々木 慶汰",
+        nameEn: "Keita Sasaki",
+        number: 7,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：175cm　/　体重：70kg　/　生年月日：1999/1/1　/　出身大学：関西学院大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_7_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_7_thumb1.jpg",
+            "images/players/handler/player_7_thumb2.jpg",
+            "images/players/handler/player_7_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "@keiter.sasaki" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 9,
+        name: "関 拓哉",
+        nameEn: "Takuya Seki",
+        number: 9,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：175cm　/　体重：65kg　/　生年月日：1994/07/02　/　出身大学：慶応義塾大学",
+        representativeCareer: [
+            "2015年　WFDFU23アルティメット選手権大会 ミックス部門",
+            "2018年　WFDFU23アルティメット選手権大会 ミックス部門",
+            "2023年　AOUGCアルティメットアジア選手権大会 オープン部門",
+            "2024年　WFDFアルティメット選手権大会 ミックス部門"
+        ],
+        playerImage: "images/players/handler/player_9_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_9_thumb1.jpg",
+            "images/players/handler/player_9_thumb2.jpg",
+            "images/players/handler/player_9_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "住友生命保険相互会社" },
+            { q: "ニックネーム", a: "団長" },
+            { q: "MBTI", a: "ENFP" },
+            { q: "得意なプレー", a: "走ること" },
+            { q: "試合前ルーティン", a: "お腹いっぱいにすること" },
+            { q: "好きな食べ物", a: "ラーメン二郎" },
+            { q: "好きな曲", a: "ポルノグラフィティ / アポロ" },
+            { q: "趣味", a: "ラーメン巡り" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ブラックマジシャンガール" },
+            { q: "インスタグラムアカウント", a: "" },
+            { q: "ひとことコメント", a: "ノマを世界一のチームに！" }
+        ]
+    },
+    {
+        id: 13,
+        name: "大久保 拓海",
+        nameEn: "Takumi Okubo",
+        number: 13,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：178cm　/　体重：72kg　/　生年月日：1999/1/1　/　出身大学：上智大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_13_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_13_thumb1.jpg",
+            "images/players/handler/player_13_thumb2.jpg",
+            "images/players/handler/player_13_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 21,
+        name: "川合 哲史",
+        nameEn: "Satoshi Kawai",
+        number: 21,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：178cm　/　体重：72kg　/　生年月日：1999/1/1　/　出身大学：上智大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_21_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_21_thumb1.jpg",
+            "images/players/handler/player_21_thumb2.jpg",
+            "images/players/handler/player_21_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 23,
+        name: "新井 康平",
+        nameEn: "Kohei Arai",
+        number: 23,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：172cm　/　体重：68kg　/　生年月日：1999/1/1　/　出身大学：慶応義塾大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_23_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_23_thumb1.jpg",
+            "images/players/handler/player_23_thumb2.jpg",
+            "images/players/handler/player_23_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 24,
+        name: "山田 和俊",
+        nameEn: "Kazutoshi Yama",
+        number: 24,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：180cm　/　体重：75kg　/　生年月日：1999/1/1　/　出身大学：早稲田大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_24_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_24_thumb1.jpg",
+            "images/players/handler/player_24_thumb2.jpg",
+            "images/players/handler/player_24_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 34,
+        name: "萬代 翔太",
+        nameEn: "Shota Mandai",
+        number: 34,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：172cm　/　体重：68kg　/　生年月日：2002/06/07　/　出身大学：関西学院大学",
+        representativeCareer: [ "2025年　WFDF世界ビーチアルティメット選手権大会 ミックス部門"],
+        playerImage: "images/players/handler/player_34_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_34_thumb1.jpg",
+            "images/players/handler/player_34_thumb2.jpg",
+            "images/players/handler/player_34_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "人事システムの営業" },
+            { q: "ニックネーム", a: "まんちゃん、まんでぃー、まんちゅう" },
+            { q: "MBTI", a: "ESFJ" },
+            { q: "得意なプレー", a: "左利きを生かしたスロー幅、バックインサイ" },
+            { q: "試合前ルーティン", a: "慣れないことしない" },
+            { q: "好きな食べ物", a: "親子丼" },
+            { q: "好きな曲", a: "SPYAIR / サムライハート" },
+            { q: "趣味", a: "アルティメット" },
+            { q: "もしも生まれ変わったら何になりたい", a: "身長が180センチ越えの人" },
+            { q: "インスタグラムアカウント", a: "@mandeeee_0607" },
+            { q: "ひとことコメント", a: "2年目も大阪から頑張ります！" }
+        ]
+    },
+    {
+        id: 37,
+        name: "陸 慶",
+        nameEn: "Kei Sako",
+        number: 37,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：180cm　/　体重：75kg　/　生年月日：1999/1/1　/　出身大学：早稲田大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_37_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_37_thumb1.jpg",
+            "images/players/handler/player_37_thumb2.jpg",
+            "images/players/handler/player_37_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 55,
+        name: "田中 拓斗",
+        nameEn: "Hiroto Tanaka",
+        number: 55,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：173cm　/　体重：69kg　/　生年月日：1999/1/1　/　出身大学：同志社大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_55_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_55_thumb1.jpg",
+            "images/players/handler/player_55_thumb2.jpg",
+            "images/players/handler/player_55_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 80,
+        name: "浜島 哲史",
+        nameEn: "Tetsuhumi Hame",
+        number: 80,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：172cm　/　体重：68kg　/　生年月日：1999/1/1　/　出身大学：慶応義塾大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_80_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_80_thumb1.jpg",
+            "images/players/handler/player_80_thumb2.jpg",
+            "images/players/handler/player_80_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    },
+    {
+        id: 83,
+        name: "伊藤 広斗",
+        nameEn: "Hiroto Ito",
+        number: 83,
+        position: "HANDLER",
+        role: "",
+        bio: "身長：173cm　/　体重：69kg　/　生年月日：1999/1/1　/　出身大学：同志社大学",
+        representativeCareer: [],
+        playerImage: "images/players/handler/player_83_thumb1.jpg",
+        qnaImages: [
+            "images/players/handler/player_83_thumb1.jpg",
+            "images/players/handler/player_83_thumb2.jpg",
+            "images/players/handler/player_83_thumb3.jpg"
+        ],
+        qna: [
+            { q: "職業", a: "ー" },
+            { q: "ニックネーム", a: "ー" },
+            { q: "MBTI", a: "ー" },
+            { q: "得意なプレー", a: "ー" },
+            { q: "試合前ルーティン", a: "ー" },
+            { q: "好きな食べ物", a: "ー" },
+            { q: "好きな曲", a: "ー" },
+            { q: "趣味", a: "ー" },
+            { q: "もしも生まれ変わったら何になりたい", a: "ー" },
+            { q: "インスタグラムアカウント", a: "ー" },
+            { q: "ひとことコメント", a: "ー" }
+        ]
+    }
+];
+
